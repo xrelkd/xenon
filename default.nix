a7cdda1557cfe5bb85ffe1ff4892f8c123710faa
xrelkd <46590321+xrelkd@users.noreply.github.com>
2022-02-21 00:35:13 +0800

    chore: bump version to `v0.2.0`

--- default.nix
+++ default.nix
@@ -4,11 +4,11 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "xenon";
-  version = "0.1.0";
+  version = "0.2.0";
 
   src = ./.;
 
-  cargoSha256 = "sha256-vVr4/S0wW1FcQxoC8N/3mLI2i0KQUoQpvyhjL8pIcCk=";
+  cargoSha256 = "sha256-Qwc4AHgYC62eJmwr+YJf0pyYPSAwY7OPkb6t8MqSBxc=";
 
   meta = with lib; {
     homepage = "https://github.com/xrelkd/xenon";
